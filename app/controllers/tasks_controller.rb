78635ba47a2225e7073feb477f77561a3364a619
Bijal Soni <bijalsoni@Bijals-MacBook-Air.local>
2016-10-20 12:43:16 +0400

    Added flash msgs, error msgs and show action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,11 +6,19 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(tasks_params)
-    @task.save
+    if @task.save
+      flash[:notice] = "Task was successfully created"
+      redirect_to task_path(@task)
 
-    redirect_to task_path(@task)
+    else
+      render 'new'
+    end
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+   
 
   private
 
