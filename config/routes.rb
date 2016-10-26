2cf43c94a77c40e23019dd814f86462a2abcbc21
Bijal Soni <bijalsoni@Bijals-MacBook-Air.local>
2016-10-26 15:05:18 +0400

    added test page and bootstrap panels

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about' => 'pages#about'
+
+  get 'test' => 'pages#test'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
