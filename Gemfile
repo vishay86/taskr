a894d926813c895576723dcfad9b92b20d9395eb
Bijal Soni <bijalsoni@Bijals-MacBook-Air.local>
2016-11-01 11:47:35 +0400

    added auto html gem

--- Gemfile
+++ Gemfile
@@ -51,3 +51,4 @@ gem "twitter-bootstrap-rails"
 gem "font-awesome-rails"
 gem 'simple_form'
 gem 'devise'
+gem 'auto_html'
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025075517) do
+ActiveRecord::Schema.define(version: 20161101073609) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.integer  "user_id"
-    t.string   "state",      default: "to_do"
+    t.string   "state",        default: "to_do"
+    t.text     "content_html"
   end
 
   create_table "users", force: :cascade do |t|
